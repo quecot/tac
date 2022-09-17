31098684684da9ac1eacdc59c169c219d651bade
Francesc Vila <francescvilasubias@gmail.com>
2022-09-17 10:00:39 +0200

    chore: add eslint airbnb config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Blog TAC</title>
-        <link rel="icon" href="/favicon.ico"></link>
-      </Head>
-      <h1>Hello world from CNA with tailwindcss</h1>
-    </div>
-  )
-}
+const Home: NextPage = () => (
+  <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <Head>
+      <title>Blog TAC</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1>Hello world from CNA with tailwindcss</h1>
+  </div>
+);
 
-export default Home
+export default Home;
