e9aeb4af5e6f7f682103136e62739a02f7001518
Francesc Vila <francescvilasubias@gmail.com>
2022-09-17 19:40:39 +0200

    feat: add post detail basic layout

---
+++ components/Comments.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Comments: React.FC = () => (
+  <div>Comments</div>
+);
+
+export default Comments;
---
+++ components/CommentsForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommentsForm: React.FC = () => (
+  <div>CommentsForm</div>
+);
+
+export default CommentsForm;
