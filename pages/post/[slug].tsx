c61636b3fabdf01e19e34b9d4e7d0b860f1b8151
Francesc Vila <francescvilasubias@gmail.com>
2022-09-18 14:28:24 +0200

    refactor: hide unimplemented components

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -13,9 +13,9 @@ const PostDetails: NextPage<{ post: Post['node']}> = ({ post }) => (
     <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
       <div className="col-span-1 lg:col-span-8">
         <PostDetail post={post} />
-        <Author author={post.author} />
+        {/* <Author author={post.author} />
         <CommentsForm slug={post.slug} />
-        <Comments slug={post.slug} />
+        <Comments slug={post.slug} /> */}
       </div>
       <div className="col-span-1 lg:col-span-4">
         <div className="relative lg:sticky top-8">
