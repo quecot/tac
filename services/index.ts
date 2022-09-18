e6a72f761aa55c91f8d78cf0e5f3a3a6c4b1aab5
Francesc Vila <francescvilasubias@gmail.com>
2022-09-18 17:10:35 +0200

    fix: desc sort recent posts

--- services/index.ts
+++ services/index.ts
@@ -77,7 +77,7 @@ export const getRecentPosts = async () => {
   const query = gql`
   query GetPostDetails() {
     posts(
-    orderBy: createdAt_ASC
+    orderBy: createdAt_DESC
     last: 3
     ) {
     title
