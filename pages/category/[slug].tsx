8677061c1baf8eb88348510317538ffe1722a038
Francesc Vila <francescvilasubias@gmail.com>
2022-09-18 14:59:55 +0200

    feat: implement categories dynamic pages

---
+++ pages/category/[slug].tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+import { getCategories, getCategoryPost } from '../../services';
+import { PostCard, Categories, Loader } from '../../components';
+import Category from '../../interfaces/Category';
+import Post from '../../interfaces/Post';
+
+interface Props {
+  posts: Post[]
+}
+
+const CategoryPost: React.FC<Props> = ({ posts }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="container px-10 mx-auto mb-8">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CategoryPost;
+
+// Fetch data at build time
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  const posts = await getCategoryPost(params.slug);
+
+  return {
+    props: { posts },
+  };
+}
+
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
+export async function getStaticPaths() {
+  const categories: Category[] = await getCategories();
+  return {
+    paths: categories.map(({ slug }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
--- services/index.ts
+++ services/index.ts
@@ -130,3 +130,40 @@ export const getCategories = async () => {
 
   return result.categories;
 };
+
+export const getCategoryPost = async (slug: string) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: {categories_some: {slug: $slug}}) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI!, query, { slug });
+
+  return result.postsConnection.edges;
+};
