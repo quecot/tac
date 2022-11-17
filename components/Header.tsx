569832828657463f8d48bd2e756244895bf1bd0b
Francesc Vila <francescvilasubias@gmail.com>
2022-11-17 18:35:18 +0100

    fix: header categories padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC = () => {
 
   return (
     <div className="container px-10 mx-auto mb-8">
-      <div className="inline-block w-full py-8 border-b border-blue-400">
+      <div className="inline-block w-full h-full py-8 border-b border-blue-400">
         <div className="block md:float-left md:pr-10">
           <Link href="/">
             <span className="text-4xl font-bold text-white cursor-pointer">
@@ -22,15 +22,17 @@ const Header: React.FC = () => {
           </Link>
         </div>
         <div className="hidden lg:float-left lg:contents">
-          {
-        categories.map((category) => (
-          <Link key={category.slug} href={`/category/${category.slug}`}>
-            <span className="mt-2 ml-4 font-semibold text-white align-middle cursor-pointer md:float-right">
-              {category.name}
-            </span>
-          </Link>
-        ))
-      }
+          <div className="w-auto pl-[20vw]">
+            {
+              categories.map((category) => (
+                <Link key={category.slug} href={`/category/${category.slug}`}>
+                  <span className="ml-4 font-semibold text-white align-middle cursor-pointer md:float-right">
+                    {category.name}
+                  </span>
+                </Link>
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
